7ccc206186fb68b25b662efd9ddd7b61a7ed8a32
alproo <programmer.2nd@gmail.com>
2023-12-26 18:18:39 +0800

    output dist

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = {
+  outputDir: 'dist',  // Set the output directory to 'dist'
   publicPath: process.env.NODE_ENV === 'production' ? '/portfolio-vuejs/' : '/',
   transpileDependencies: true,
 };
