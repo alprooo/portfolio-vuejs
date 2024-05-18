e5e7d45b5e4e9f673c8a84b7e34e1fed36a4a02e
alproo <programmer.2nd@gmail.com>
2024-05-18 18:45:33 +0800

    change image

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -49,7 +49,7 @@
           </div>
           <div class="avatar">
             <div class="image">
-              <img src="/img/slider/avatar.png" alt="" />
+              <img src="/img/slider/avatar2.png" alt="" />
               <span
                 class="skills illustrator anim_moveBottom"
                 v-html="ai"
